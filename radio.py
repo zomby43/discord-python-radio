48734077c92f547b8b6db0e5465089798ffd6581
zomby43 <psycho432@gmail.com>
2024-08-18 03:32:22 -0400

    removed some unused libraries

--- radio.py
+++ radio.py
@@ -3,10 +3,6 @@ from discord import app_commands
 from discord.ext import commands
 import asyncio
 import random
-import requests
-import io
-import aiohttp
-import pydub
 import logging
 
 intents = discord.Intents.default()
